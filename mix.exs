b41166cedc22ac897bff6ce4043cebd43188f57c
Spencer Creasey <spence@users.noreply.github.com>
2017-02-12 14:46:28 -0800

    chore: patch version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ValidUrl.Mixfile do
 
   def project do
     [app: :valid_url,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
